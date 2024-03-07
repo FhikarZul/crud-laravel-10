0120ebacfc70161cd4ae357f44217ea2cd41530a
Fhikar Zul <Fhikarzul18@gmail.com>
2024-03-07 21:38:44 +0800

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Http\Controllers;
+//import Model "Post
+use App\Models\Post;
+
+//return type View
+use Illuminate\View\View;
+
+class PostController extends Controller
+{
+    /**
+     * index
+     *
+     * @return View
+     */
+    public function index(): View
+    {
+        //get posts
+        $posts = Post::latest()->paginate(5);
+
+        //render view with posts
+        return view('posts.index', compact('posts'));
+    }
+}
+
